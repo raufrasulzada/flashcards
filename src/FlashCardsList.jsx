d27b828c3576dbaa486c5b7938b3db76a5e9e52c
Rauf Rasulzada <rrasulzada155558@ada.edu.az>
2023-12-17 16:50:19 +0400

    Search, filter, sort

--- src/FlashCardsList.jsx
+++ src/FlashCardsList.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+// Update FlashCardsList.jsx
+
+import React, { useState } from "react";
 import FlashCards from "./FlashCards";
 import CreateCard from "./CreateCard";
 
@@ -9,6 +11,10 @@ function FlashCardsList({
   setCreateCardModalOpen,
   setFlashCards,
 }) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filterStatus, setFilterStatus] = useState("All");
+  const [sortAttribute, setSortAttribute] = useState("id");
+
   const handleEdit = (id, front, back) => {
     console.log("Editing card:", id, front, back);
   };
@@ -18,9 +24,58 @@ function FlashCardsList({
     setFlashCards((prevFlashCards) => [...prevFlashCards, newCard]);
   };
 
+  const filteredCards = flashCards
+    .filter(
+      (card) =>
+        card.front.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        card.back.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    .filter((card) =>
+      filterStatus === "All"
+        ? true
+        : card.status &&
+          card.status.toLowerCase() === filterStatus.toLowerCase()
+    )
+    .sort((a, b) => {
+      if (sortAttribute === "id") {
+        return a.id - b.id;
+      } else if (sortAttribute === "front") {
+        return a.front.localeCompare(b.front);
+      } else if (sortAttribute === "back") {
+        return a.back.localeCompare(b.back);
+      }
+      return 0;
+    });
+
+  console.log("Filter Status:", filterStatus);
+  console.log("Filtered Cards:", filteredCards);
+
   return (
     <div className="card-container">
-      {flashCards.map((flashCard) => (
+      <input
+        type="text"
+        placeholder="Search cards..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <select
+        value={filterStatus}
+        onChange={(e) => setFilterStatus(e.target.value)}
+      >
+        <option value="All">All</option>
+        <option value="Want to Learn">Want to Learn</option>
+        <option value="Noted">Noted</option>
+        <option value="Learned">Learned</option>
+      </select>
+      <select
+        value={sortAttribute}
+        onChange={(e) => setSortAttribute(e.target.value)}
+      >
+        <option value="id">ID</option>
+        <option value="front">Front</option>
+        <option value="back">Back</option>
+      </select>
+      {filteredCards.map((flashCard) => (
         <FlashCards
           key={flashCard.id}
           flashCard={flashCard}
